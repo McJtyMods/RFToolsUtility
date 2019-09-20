ead0ce73cbae64c211f5fbaa92fb017d8aa5dd27
McJty <slto15aGH10>
2019-09-20 07:49:23 +0200

    Added recipes, loot tables and advancements

--- src/main/java/mcjty/rftoolsutility/datagen/Recipes.java
+++ src/main/java/mcjty/rftoolsutility/datagen/Recipes.java
@@ -2,6 +2,7 @@ package mcjty.rftoolsutility.datagen;
 
 import mcjty.rftoolsbase.items.ModItems;
 import mcjty.rftoolsutility.modules.crafter.CrafterSetup;
+import mcjty.rftoolsutility.modules.teleporter.TeleporterSetup;
 import net.minecraft.advancements.criterion.InventoryChangeTrigger;
 import net.minecraft.block.Blocks;
 import net.minecraft.data.DataGenerator;
@@ -49,6 +50,59 @@ public class Recipes extends RecipeProvider {
                 .key('M', CrafterSetup.BLOCK_CRAFTER2)
                 .setGroup("")
                 .addCriterion("crafter2", InventoryChangeTrigger.Instance.forItems(CrafterSetup.BLOCK_CRAFTER2))
+                .build(consumer);
+
+        ShapedRecipeBuilder.shapedRecipe(TeleporterSetup.DIALING_DEVICE)
+                .patternLine("rrr")
+                .patternLine("TMT")
+                .patternLine("rrr")
+                .key('r', Items.REDSTONE)
+                .key('T', Blocks.REDSTONE_TORCH)
+                .key('M', ModItems.MACHINE_FRAME)
+                .setGroup("")
+                .addCriterion("frame", InventoryChangeTrigger.Instance.forItems(ModItems.MACHINE_FRAME))
+                .build(consumer);
+        ShapedRecipeBuilder.shapedRecipe(TeleporterSetup.MATTER_RECEIVER)
+                .patternLine("iii")
+                .patternLine("rMr")
+                .patternLine("ooo")
+                .key('r', Items.REDSTONE)
+                .key('i', Items.IRON_INGOT)
+                .key('o', Items.ENDER_PEARL)
+                .key('M', ModItems.MACHINE_FRAME)
+                .setGroup("")
+                .addCriterion("frame", InventoryChangeTrigger.Instance.forItems(ModItems.MACHINE_FRAME))
+                .build(consumer);
+        ShapedRecipeBuilder.shapedRecipe(TeleporterSetup.MATTER_TRANSMITTER)
+                .patternLine("ooo")
+                .patternLine("rMr")
+                .patternLine("iii")
+                .key('r', Items.REDSTONE)
+                .key('i', Items.IRON_INGOT)
+                .key('o', Items.ENDER_PEARL)
+                .key('M', ModItems.MACHINE_FRAME)
+                .setGroup("")
+                .addCriterion("frame", InventoryChangeTrigger.Instance.forItems(ModItems.MACHINE_FRAME))
+                .build(consumer);
+        ShapedRecipeBuilder.shapedRecipe(TeleporterSetup.MATTER_BOOSTER)
+                .patternLine(" B ")
+                .patternLine("BMB")
+                .patternLine(" B ")
+                .key('B', Blocks.REDSTONE_BLOCK)
+                .key('M', ModItems.MACHINE_FRAME)
+                .setGroup("")
+                .addCriterion("frame", InventoryChangeTrigger.Instance.forItems(ModItems.MACHINE_FRAME))
+                .build(consumer);
+        ShapedRecipeBuilder.shapedRecipe(TeleporterSetup.SIMPLE_DIALER)
+                .patternLine("rRr")
+                .patternLine("TMT")
+                .patternLine("rRr")
+                .key('r', Items.REDSTONE)
+                .key('R', Blocks.REDSTONE_BLOCK)
+                .key('T', Blocks.REDSTONE_TORCH)
+                .key('M', ModItems.MACHINE_BASE)
+                .setGroup("")
+                .addCriterion("frame", InventoryChangeTrigger.Instance.forItems(ModItems.MACHINE_BASE))
                 .build(consumer);
 
     }
