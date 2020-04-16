4a0013eac543cd2ca00be86e65bb69173d65e15a
McJty <mcjty1@gmail.com>
2020-04-16 06:39:04 +0200

    Many fixes

--- src/main/java/mcjty/rftoolsutility/modules/teleporter/data/TeleportDestination.java
+++ src/main/java/mcjty/rftoolsutility/modules/teleporter/data/TeleportDestination.java
@@ -4,6 +4,8 @@ import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.dimension.DimensionType;
 
+import java.util.Objects;
+
 public class TeleportDestination {
     private final BlockPos coordinate;
     private final DimensionType dimension;
@@ -67,33 +69,16 @@ public class TeleportDestination {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         TeleportDestination that = (TeleportDestination) o;
-
-        if (!dimension.equals(that.dimension)) {
-            return false;
-        }
-        if (coordinate != null ? !coordinate.equals(that.coordinate) : that.coordinate != null) {
-            return false;
-        }
-        if (name != null ? !name.equals(that.name) : that.name != null) {
-            return false;
-        }
-
-        return true;
+        return Objects.equals(coordinate, that.coordinate) &&
+                Objects.equals(dimension, that.dimension) &&
+                Objects.equals(name, that.name);
     }
 
     @Override
     public int hashCode() {
-        int result = coordinate != null ? coordinate.hashCode() : 0;
-        result = 31 * result + dimension.getId();
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
+        return Objects.hash(coordinate, dimension, name);
     }
 }
