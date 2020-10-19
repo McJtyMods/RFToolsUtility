49e6ce4719398f43bb487a7ba27852fd7787e07c
mcjty <slto15aGH10>
2020-10-19 11:04:35 +0200

    Various fixes to upgraded crafting and infusion of shield projectors

--- src/main/java/mcjty/rftoolsutility/modules/crafter/blocks/CrafterBlock.java
+++ src/main/java/mcjty/rftoolsutility/modules/crafter/blocks/CrafterBlock.java
@@ -86,6 +86,6 @@ public class CrafterBlock extends BaseBlock implements INBTPreservingIngredient 
 
     @Override
     public Collection<String> getTagsToPreserve() {
-        return Collections.singleton("Info");
+        return Collections.singleton("BlockEntityTag");
     }
 }
