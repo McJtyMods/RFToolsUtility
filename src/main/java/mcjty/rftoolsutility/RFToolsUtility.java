9716a082e84176258788d9e9897d26473b6fea87
McJty <mcjty1@gmail.com>
2021-01-27 18:58:34 +0100

    Various things

--- src/main/java/mcjty/rftoolsutility/RFToolsUtility.java
+++ src/main/java/mcjty/rftoolsutility/RFToolsUtility.java
@@ -1,6 +1,8 @@
 package mcjty.rftoolsutility;
 
 import mcjty.lib.modules.Modules;
+import mcjty.rftoolsbase.api.teleportation.ITeleportationManager;
+import mcjty.rftoolsutility.apiimpl.teleportation.TeleportationManager;
 import mcjty.rftoolsutility.modules.crafter.CrafterModule;
 import mcjty.rftoolsutility.modules.logic.LogicBlockModule;
 import mcjty.rftoolsutility.modules.screen.ScreenModule;
@@ -15,7 +17,11 @@ import mcjty.rftoolsutility.setup.Registration;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 @Mod(RFToolsUtility.MODID)
 public class RFToolsUtility {
@@ -38,10 +44,20 @@ public class RFToolsUtility {
 
         FMLJavaModLoadingContext.get().getModEventBus().addListener(setup::init);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(modules::init);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
 
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
             FMLJavaModLoadingContext.get().getModEventBus().addListener(modules::initClient);
             FMLJavaModLoadingContext.get().getModEventBus().addListener(ClientSetup::onTextureStitch);
+        });
+    }
+
+    private void processIMC(final InterModProcessEvent event) {
+        event.getIMCStream().forEach(message -> {
+            if ("getTeleportationManager".equals(message.getMethod())) {
+                Supplier<Function<ITeleportationManager, Void>> supplier = message.getMessageSupplier();
+                supplier.get().apply(new TeleportationManager());
+            }
         });
     }
 
