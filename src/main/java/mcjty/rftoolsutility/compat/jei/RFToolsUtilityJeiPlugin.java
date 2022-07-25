e19ec1d5d87d798e4b36c75ab64542efbd946f91
McJty <mcjty1@gmail.com>
2022-07-25 07:12:46 +0200

    More porting work

--- src/main/java/mcjty/rftoolsutility/compat/jei/RFToolsUtilityJeiPlugin.java
+++ src/main/java/mcjty/rftoolsutility/compat/jei/RFToolsUtilityJeiPlugin.java
@@ -5,6 +5,7 @@ import mcjty.rftoolsutility.RFToolsUtility;
 import mcjty.rftoolsutility.setup.RFToolsUtilityMessages;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.ingredient.IRecipeSlotView;
 import mezz.jei.api.ingredients.ITypedIngredient;
 import mezz.jei.api.registration.IRecipeTransferRegistration;
@@ -28,7 +29,7 @@ public class RFToolsUtilityJeiPlugin implements IModPlugin {
         for (int i = 0 ; i < slotViews.size() ; i++) {
             List<ITypedIngredient<?>> allIngredients = slotViews.get(i).getAllIngredients().collect(Collectors.toList());
             if (!allIngredients.isEmpty()) {
-                ItemStack stack = (ItemStack) allIngredients.get(0).getIngredient();    // @todo 1.19 is this correct?
+                ItemStack stack = allIngredients.get(0).getIngredient(VanillaTypes.ITEM_STACK).get();
                 items.set(i, stack);
             }
         }
